e3f57867a9e6062d82f28f0baaa74a2bebe20a20
salogranada <salogranada494@gmail.com>
2023-02-07 20:36:31 +0000

    UPT: launch file

--- launch/vision.launch.py
+++ launch/vision.launch.py
@@ -1,37 +1,28 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Launch a talker and a listener in a component container."""
+"""Launch the vision stack in a component container."""
 
 import launch
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import LoadComposableNodes, Node
 from launch_ros.descriptions import ComposableNode
+from launch import LaunchDescription
 
 
 def generate_launch_description():
     """Generate launch description with multiple components."""
-    container = ComposableNodeContainer(
-        name="vision_kronos",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="cv_camera", plugin="cv_camera::Driver", name="driver"
+    return LaunchDescription(
+        [
+            Node(
+                name="vision_kronos",
+                package="rclcpp_components",
+                executable="component_container",
+                output="both",
             ),
-        ],
-        output="screen",
+            LoadComposableNodes(
+                target_container="vision_kronos",
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package="cv_camera", plugin="cv_camera::Driver", name="driver"
+                    ),
+                ],
+            ),
+        ]
     )
-
-    return launch.LaunchDescription([container])
