b6a16d035df06a2ad603d96b909fbe3275bf00d5
salogranada <salogranada494@gmail.com>
2023-02-09 22:43:12 +0000

    FEAT: use param file for launch

--- launch/vision.launch.py
+++ launch/vision.launch.py
@@ -1,97 +1,59 @@
 """Launch the vision stack in a component container."""
-
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import LoadComposableNodes, Node
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import (
+    DeclareLaunchArgument,
+    SetEnvironmentVariable,
+    GroupAction,
+)
+from launch.conditions import IfCondition
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
-import cv2
 
-config_path = "/workspace/rover/ros2/src/vision_cpp/cv_camera/launch/camera_info.yaml"
-
-camera_names_ports = {
-    "left": "3-4.4.4:1.0",
-    "right": "3-4.4.3:1.0",
-    # "back": "1-1.5:1.0",
-    # "zoom": "1-1.6:1.0",
-}
-
-video_format = "MJPG"
+camera_names_ports = ["left", "right", "back", "zoom"]
 
 
 def generate_launch_description():
 
-    """Declare parameters"""
-    publish_rate = LaunchConfiguration("publish_rate")
-    declare_publish_rate_cmd = DeclareLaunchArgument(
-        "publish_rate",
-        default_value="10.0",
-        description="Rate of publish images",
+    config = os.path.join(
+        get_package_share_directory("cv_camera"), "launch", "cv_camera_params.yaml"
     )
-    camera_info_url = LaunchConfiguration("camera_info_url")
-    declare_camera_info_url_cmd = DeclareLaunchArgument(
-        "camera_info_url",
-        default_value=config_path,
-        description="path to the camera info file",
-    )
-    read_rate = LaunchConfiguration("read_rate")
-    declare_read_rate_cmd = DeclareLaunchArgument(
-        "read_rate",
-        default_value="30.0",
-        description="Rate of read images",
-    )
-    cam_format = LaunchConfiguration("cv_cap_prop_fourcc")
-    declare_cam_format_cmd = DeclareLaunchArgument(
-        "cv_cap_prop_fourcc",
-        default_value=str(float(cv2.VideoWriter_fourcc(*video_format))),
-        description="Format to open the cameras",
-    )
-    # remappings = [
-    #     ("/left/image_raw", "/video_mapping/left"),
-    #     ("/right/image_raw", "/video_mapping/right"),
-    #     ("/back/image_raw", "/video_mapping/back"),
-    #     ("/zoom/image_raw", "/video_mapping/zoom"),
-    # ]
 
     """Iter through available cameras"""
     nodes = []
-    for camera_name, port in camera_names_ports.items():
+    for camera_name in camera_names_ports:
         node = ComposableNode(
-            parameters=[
-                {"device_path": port},
-                {"image_width": 640},
-                {"image_height": 360},
-                {"publish_rate": publish_rate},
-                {"read_rate": read_rate},
-                {"cv_cap_prop_fourcc": cam_format},
-                {"frame_id": "camera"},
-                {"camera_info_url": camera_info_url},
-                {"topic_name": "/video_mapping/" + camera_name},
-            ],
+            parameters=[config],
             package="cv_camera",
             plugin="cv_camera::Driver",
             namespace=camera_name,
             name=f"cv_camera_{camera_name}",
-            # remappings=remappings,
         )
         nodes.append(node)
-
-    """Create the launch description"""
     return LaunchDescription(
         [
-            declare_publish_rate_cmd,
-            declare_read_rate_cmd,
-            declare_camera_info_url_cmd,
-            declare_cam_format_cmd,
-            Node(
-                name="vision_kronos",
-                package="rclcpp_components",
-                executable="component_container",
-                output="both",
+            # -------------- COMPOSITION -------------------------------
+            DeclareLaunchArgument(
+                "use_composition",
+                default_value="True",
+                description="Whether to use node composition",
             ),
-            LoadComposableNodes(
-                target_container="vision_kronos",
-                composable_node_descriptions=[*nodes],
+            GroupAction(
+                condition=IfCondition(LaunchConfiguration("use_composition")),
+                actions=[
+                    Node(
+                        name="vision_kronos",
+                        package="rclcpp_components",
+                        executable="component_container",
+                        output="both",
+                    ),
+                    LoadComposableNodes(
+                        target_container="vision_kronos",
+                        composable_node_descriptions=[*nodes],
+                    ),
+                ],
             ),
         ]
     )
